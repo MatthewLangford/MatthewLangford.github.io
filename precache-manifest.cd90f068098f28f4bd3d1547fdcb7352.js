567fb24bdaf4b9bca491e4c93fba248571eef170
Matthew <m.c.langford@live.com>
2018-12-06 12:26:13 -0700

    initial commit

---
+++ precache-manifest.cd90f068098f28f4bd3d1547fdcb7352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfb2f44a7c1deba39f7f4d39bff18eeb",
+    "url": "/static/media/DroidSerif-Regular.bfb2f44a.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b0995f35c78530a9714",
+    "url": "/static/js/main.6b0995f3.chunk.js"
+  },
+  {
+    "revision": "ef05c8db220b699a6ca3",
+    "url": "/static/js/1.ef05c8db.chunk.js"
+  },
+  {
+    "revision": "6b0995f35c78530a9714",
+    "url": "/static/css/main.d7a2fb49.chunk.css"
+  },
+  {
+    "revision": "185a651272ff2e96a7b40d34067ea500",
+    "url": "/index.html"
+  }
+];
